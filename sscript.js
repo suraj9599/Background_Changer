73348d95794feaa6d5b06cd966973158712cb631
Suraj Singh <110949425+suraj9599@users.noreply.github.com>
2024-07-25 14:26:53 +0530

    Update script.js

--- sscript.js
+++ sscript.js
@@ -1,19 +1,20 @@
 const buttons = document.querySelectorAll(".buttons");
 const body = document.querySelector("body");
+const container = document.querySelector(".container");
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
     if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
     }
     if (e.target.id === "yellow") {
-      body.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
     }
     if (e.target.id === "red") {
-      body.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
     }
     if (e.target.id === "green") {
-      body.style.backgroundColor = e.target.id;
+      container.style.backgroundColor = e.target.id;
     }
   });
 });
